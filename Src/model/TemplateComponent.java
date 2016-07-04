08c9a04f1b1b747475b8190292abd9fa060a2b75
JCYYYY <icysomnus@gmail.com>
2016-07-04 17:32:55 +0800

    Update TemplateComponent.java

--- Src/model/TemplateComponent.java
+++ Src/model/TemplateComponent.java
@@ -11,26 +11,44 @@ import com.example.mfusion.Utility.ScreenUtil;
  * Created by JCYYYY on 2016/5/26.
  */
 public class TemplateComponent implements Parcelable{
+    private int id;
+
+
+
     private float left;
     private float top;
     private float right;
     private float bottom;
+    private int tid;
+
+
+
+
 
     //newwwwww
     public static final int TYPE_TEXT=1;
     public static final int TYPE_VIDEO=2;
     public static final int TYPE_IMAGE=3;
+    public static final int TYPE_STATIC_TEXT=4;
 
 
     private int type;
     private Uri sourceUri;
     private String sourceText;
 
+    private String fontType;
+    private float fontSize;
+    private String fontColor;
+    private int fontStyle;
+
     private Context context;
     private ScreenUtil su;
 
     private int parentWidth;
     private int parentHeight;
+
+
+
 
     public int getParentWidth() {
         return parentWidth;
@@ -71,7 +89,22 @@ public class TemplateComponent implements Parcelable{
     public void setSourceText(String sourceText) {
         this.sourceText = sourceText;
     }
-//----
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    //----
 
 
 
@@ -122,7 +155,39 @@ public class TemplateComponent implements Parcelable{
         this.bottom = bottom;
     }
 
-    public TemplateComponent(Context context,float left, float top, float right, float bottom) {
+    public String getFontType() {
+        return fontType;
+    }
+
+    public void setFontType(String fontType) {
+        this.fontType = fontType;
+    }
+
+    public float getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(float fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public String getFontColor() {
+        return fontColor;
+    }
+
+    public void setFontColor(String fontColor) {
+        this.fontColor = fontColor;
+    }
+
+    public int getFontStyle() {
+        return fontStyle;
+    }
+
+    public void setFontStyle(int fontStyle) {
+        this.fontStyle = fontStyle;
+    }
+
+    public TemplateComponent(Context context, float left, float top, float right, float bottom) {
 
         this.left = left;
         this.top = top;
@@ -138,13 +203,22 @@ public class TemplateComponent implements Parcelable{
 
     //------------prece
     protected TemplateComponent(Parcel in) {
+        id=in.readInt();
         left = in.readFloat();
         top = in.readFloat();
         right = in.readFloat();
         bottom = in.readFloat();
+        tid=in.readInt();
+
         type = in.readInt();
+
+
         sourceUri = (Uri) in.readValue(Uri.class.getClassLoader());
         sourceText = in.readString();
+        fontType=in.readString();
+        fontSize=in.readFloat();
+        fontColor=in.readString();
+        fontStyle=in.readInt();
 
         parentWidth = in.readInt();
         parentHeight = in.readInt();
@@ -157,20 +231,27 @@ public class TemplateComponent implements Parcelable{
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
         dest.writeFloat(left);
         dest.writeFloat(top);
         dest.writeFloat(right);
         dest.writeFloat(bottom);
+        dest.writeInt(tid);
+
         dest.writeInt(type);
         dest.writeValue(sourceUri);
         dest.writeString(sourceText);
+        dest.writeString(fontType);
+        dest.writeFloat(fontSize);
+        dest.writeString(fontColor);
+        dest.writeInt(fontStyle);
 
         dest.writeInt(parentWidth);
         dest.writeInt(parentHeight);
     }
 
     @SuppressWarnings("unused")
-    public static final Parcelable.Creator<TemplateComponent> CREATOR = new Parcelable.Creator<TemplateComponent>() {
+    public static final Creator<TemplateComponent> CREATOR = new Creator<TemplateComponent>() {
         @Override
         public TemplateComponent createFromParcel(Parcel in) {
             return new TemplateComponent(in);
