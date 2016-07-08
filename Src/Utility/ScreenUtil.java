3895742864332f22d386b2cbae6323c302f57677
WangMengNi <hansey20@hotmail.com>
2016-07-08 15:54:28 +0800

    Update ScreenUtil.java

--- Src/Utility/ScreenUtil.java
+++ Src/Utility/ScreenUtil.java
@@ -4,9 +4,7 @@ import android.content.Context;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
-/**
- * Created by 1B15182 on 5/5/2016.
- */
+
 public class ScreenUtil {
     private Context context;
     private DisplayMetrics dm;
