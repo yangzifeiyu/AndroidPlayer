bd429d890bde10a10790d6abd0613c7265b25df0
JCYYYY <icysomnus@gmail.com>
2016-07-08 15:21:39 +0800

    Update TemplateDAO.java

--- Src/Template/TemplateDAO.java
+++ Src/Template/TemplateDAO.java
@@ -45,11 +45,25 @@ public class TemplateDAO {
             currentComponent.setId(result.getInt(5));
             componentList.add(currentComponent);
         }
+        String selectTemplateNameSql="select name from "+DbIniter.TABLE_NAME_TEMPLATE+" where id="+id;
+        Cursor nameResult=db.rawQuery(selectTemplateNameSql,null);
+        nameResult.moveToLast();
+        String templateName=nameResult.getString(0);
+
         MyTemplate myTemplate = new MyTemplate();
         myTemplate.setList(componentList);
         myTemplate.setId(id);
+        myTemplate.setName(templateName);
         return myTemplate;
 
+    }
+    public List<MyTemplate> getAllTemplate(){
+        int largestId=getLargestTemplateId();
+        List<MyTemplate> list=new ArrayList<>();
+        for(int i=1;i<=largestId;i++){
+            list.add(getMyTemplateById(i));
+        }
+        return list;
     }
 
     public void updateMyTemplete(MyTemplate updatedTemplate) {
@@ -72,6 +86,38 @@ public class TemplateDAO {
 
     }
 
+    public void saveTemplate(MyTemplate newTemplate){
+        String sql="insert into "+DbIniter.TABLE_NAME_TEMPLATE+" values(null,'"+newTemplate.getName()+"')";
+        Log.i(TAG, "saveTemplate: create new template sql="+sql);
+        db.beginTransaction();
+        db.execSQL(sql);
+        db.setTransactionSuccessful();
+        db.endTransaction();
+        int lastestId=getLargestTemplateId();
+        List<TemplateComponent> components=newTemplate.getList();
+        for(TemplateComponent current:components){
+            String insertComponentSql="insert into "+DbIniter.TABLE_NAME_COMPONENT+" (id,left,top,right,bottom,tid) values(null,"+current.getLeft()+","+current.getTop()+","+current.getRight()+","+current.getBottom()+","+lastestId+")";
+            Log.i(TAG, "saveTemplate: insert component sql="+insertComponentSql);
+            db.beginTransaction();
+            db.execSQL(insertComponentSql);
+            db.setTransactionSuccessful();
+            db.endTransaction();
+        }
+
+
+    }
+    private int getLargestTemplateId(){
+        String sql="select id from "+DbIniter.TABLE_NAME_TEMPLATE+" order by id desc limit 1";
+        Cursor result=db.rawQuery(sql,null);
+        result.moveToLast();
+        int largestId=result.getInt(0);
+        result.close();
+        Log.i(TAG, "getLargestTemplateId: result="+largestId);
+        return largestId;
+    }
+
+
+
     public void createNewUserScreen(MyTemplate template){
         List<TemplateComponent> components=template.getList();
         String createScreenSql="insert into "+ DbIniter.TABLE_NAME_USER_SCREEN+" values(null,'"+template.getName()+"')";
@@ -86,7 +132,8 @@ public class TemplateDAO {
             if(current.getSourceUri()!=null)
                 uriStr=current.getSourceUri().toString();
 
-            String createUserScreenComponentSql="insert into "+ DbIniter.TABLE_NAME_USER_SCREEN_COMPONENT+" values(null,"+current.getLeft()+","+current.getTop()+","+current.getRight()+","+current.getBottom()+","+template.getId()+","+current.getType()+",'"+uriStr+"','"+current.getSourceText()+"','"+current.getFontType()+"',"+current.getFontSize()+",'"+current.getFontColor()+"',"+current.getFontStyle()+")";
+            String createUserScreenComponentSql="insert into "+ DbIniter.TABLE_NAME_USER_SCREEN_COMPONENT+" values(null,"+current.getLeft()+","+current.getTop()+","+current.getRight()+","+current.getBottom()+","+template.getId()+","+current.getType()+",'"+uriStr+"','"+current.getSourceText().replace("'","''")+"','"+current.getFontType()+"',"+current.getFontSize()+",'"+current.getFontColor()+"',"+current.getFontStyle()+")";
+
             Log.i(TAG, "createNewUserScreen: createUserScreenComponentSql="+createUserScreenComponentSql);
             db.beginTransaction();
             db.execSQL(createUserScreenComponentSql);
