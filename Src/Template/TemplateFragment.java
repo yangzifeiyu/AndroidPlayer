352ed1297545669761e74a6049ace0be007c78f0
JCYYYY <icysomnus@gmail.com>
2016-07-08 15:22:24 +0800

    Update TemplateFragment.java

--- Src/Template/TemplateFragment.java
+++ Src/Template/TemplateFragment.java
@@ -8,18 +8,26 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.mfusion.EditTextPropertyActivity;
 import com.example.mfusion.R;
 import com.example.mfusion.ShowTemplateActivity;
+import com.example.mfusion.adapter.TemplateGridViewAdapter;
 import com.example.mfusion.model.MyTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
-public class TemplateFragment extends Fragment implements View.OnClickListener{
+public class TemplateFragment extends Fragment{
 
 	private TemplateViewNew templateViewNew;
 
@@ -27,139 +35,72 @@ public class TemplateFragment extends Fragment implements View.OnClickListener{
 	private ImageView btnPrevious;
 	private ViewPager viewPager;
 	private static int currentPosition;
-	private ArrayList<MyTemplate> templates;
+	private List<MyTemplate> templates;
 
 	private TemplateDAO tDAO;
 	private static final String TAG="TemplateFragment";
 
+	@BindView(R.id.template_list_gridview)GridView gvList;
+	@BindView(R.id.template_list_img_new_template)ImageView imgNewTemplate;
 	@Override
 	public View onCreateView(LayoutInflater inflater, final ViewGroup container,
 							 Bundle savedInstanceState) {
 
-		View rootView = inflater.inflate(R.layout.fragment_template, container, false);
-
+		View rootView = inflater.inflate(R.layout.activity_test_template_list, container, false);
+		ButterKnife.bind(this,rootView);
 
 		templateViewNew = (TemplateViewNew) rootView.findViewById(R.id.templateView);
-		btnNext = (ImageView) rootView.findViewById(R.id.btnNext);
-		btnPrevious = (ImageView) rootView.findViewById(R.id.btnPrevious);
+//		btnNext = (ImageView) rootView.findViewById(R.id.btnNext);
+//		btnPrevious = (ImageView) rootView.findViewById(R.id.btnPrevious);
 
 		viewPager = (ViewPager) getActivity().findViewById(R.id.pager);
 
 		tDAO = new TemplateDAO(getActivity());
 
-		templates = new ArrayList<>();
-		templates.add(tDAO.getMyTemplateById(0));
-		templates.add(tDAO.getMyTemplateById(1));
-		templates.add(tDAO.getMyTemplateById(2));
-		templates.add(tDAO.getMyTemplateById(3));
-		templates.add(tDAO.getMyTemplateById(4));
-		templates.add(tDAO.getMyTemplateById(5));
-//		templates.add(new MyTemplate(new TemplateComponent(0,0,0.5f,1), new TemplateComponent(0.5f,0.5f,1,1), new TemplateComponent(0.5f,0,1,0.5f)));
-//		templates.add(new MyTemplate(new TemplateComponent(0,0,1,0.2f),new TemplateComponent(0,0.2f,0.5f,1),new TemplateComponent(0.5f,0.2f,1,1)));
-//		templates.add(new MyTemplate(new TemplateComponent(0,0,0.5f,0.5f),new TemplateComponent(0.5f,0,1,0.5f),new TemplateComponent(0,0.5f,0.5f,1),new TemplateComponent(0.5f,0.5f,1,1)));
-//
-//		currentPosition=0;
-//		templateView.addTemplate(templates.get(currentPosition));
+        updateAdapter();
 
-		templateViewNew.setBackgroundColor(Color.LTGRAY);
-
-		btnPrevious.setOnClickListener(this);
-		btnNext.setOnClickListener(this);
-
-//		templateView.setOnLongClickListener(new View.OnLongClickListener() {
-//			@Override
-//			public boolean onLongClick(View v) {
-//				Intent goShow=new Intent(container.getContext(),ShowTemplateActivity.class);
-//				goShow.putExtra("tid",templates.get(currentPosition).getId());
-//				startActivity(goShow);
-//				return true;
-//			}
-//		});
-
-		templateViewNew.setOnClickListener(new View.OnClickListener() {
+		gvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
-			public void onClick(View v) {
-				Intent goShow=new Intent(container.getContext(),ShowTemplateActivity.class);
-				goShow.putExtra("tid",templates.get(currentPosition).getId());
+			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+				Intent goShow= new Intent(container.getContext(),ShowTemplateActivity.class);
+				goShow.putExtra("tid",templates.get(i).getId());
 				startActivity(goShow);
-				return;
+
 			}
 		});
 
-		templateViewNew.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Intent goEditTextProp=new Intent(getActivity(),EditTextPropertyActivity.class);
 
-
-				startActivity(goEditTextProp);
-
-				return false;
-			}
-		});
-
-//		templateView.setOnLongClickListener(new View.OnLongClickListener() {
-//			@Override
-//			public boolean onLongClick(View v) {
-//				Intent goTest=new Intent(container.getContext(), TestActivity.class);
-//				goTest.putExtra("tid",templates.get(currentPosition).getId());
-//				startActivity(goTest);
-//				return false;
-//			}
-//		});
-
-
-//		templateView.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				Log.i(TAG, "onClick: triggered");
-//			}
-//		});
 
 		return rootView;
 
 	}
+    private void updateAdapter(){
+        templates=tDAO.getAllTemplate();
 
 
-
-
-
-
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        currentPosition=0;
-        templateViewNew.setTemplate(templates.get(currentPosition));
+        TemplateGridViewAdapter adapter=new TemplateGridViewAdapter(getActivity(),templates);
+        gvList.setAdapter(adapter);
     }
 
-    public void onClick(View v) {
-		switch (v.getId()){
-			case R.id.btnNext:
-				//templateView.clearComponent();
-				currentPosition++;
-				checkCounter();
-				templateViewNew.setTemplate(templates.get(currentPosition));
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateAdapter();
+    }
 
-				break;
-			case R.id.btnPrevious:
-				//templateView.clearComponent();
-				currentPosition--;
-				checkCounter();
-				templateViewNew.setTemplate(templates.get(currentPosition));
-				break;
-		}
-
+    @OnClick(R.id.template_list_img_new_template)
+	void newTemplate(){
+        Intent goAddTemplate=new Intent(getActivity(),AddNewTemplateActivity.class);
+        startActivity(goAddTemplate);
 	}
 
-	private void checkCounter() {
-		int maxIndex=templates.size()-1;
-		if (currentPosition>maxIndex)
-			currentPosition=0;
-		if (currentPosition<0) {
-			currentPosition = maxIndex;
-		}
-	}
+
+
+
+
+
+
+
+
 
 }
