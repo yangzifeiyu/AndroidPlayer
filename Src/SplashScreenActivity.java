e8342eae8b07fd0e83acc4ad35af8262208bfa0e
WangMengNi <hansey20@hotmail.com>
2016-07-12 17:09:26 +0800

    Add files via upload

---
+++ Src/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.example.mfusion;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.move_up);
+        ImageView imageView= (ImageView) findViewById(R.id.imgSplash);
+        imageView.setAnimation(anim);
+
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        },4000);
+
+
+    }
+}
