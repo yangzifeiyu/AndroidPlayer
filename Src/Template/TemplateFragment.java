2ec02dadfe5c6c62b92c4761974914caebecb563
JCYYYY <icysomnus@gmail.com>
2016-07-19 10:02:18 +0800

    Update TemplateFragment.java

--- Src/Template/TemplateFragment.java
+++ Src/Template/TemplateFragment.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.mfusion.EditTextPropertyActivity;
 import com.example.mfusion.R;
@@ -42,6 +44,11 @@ public class TemplateFragment extends Fragment{
 
 	@BindView(R.id.template_list_gridview)GridView gvList;
 	@BindView(R.id.template_list_img_new_template)ImageView imgNewTemplate;
+	@BindView(R.id.template_list_minus)ImageView imgMinus;
+	@BindView(R.id.template_list_img_delete)ImageView imgDelete;
+	private TemplateGridViewAdapter adapter;
+	private boolean deleteModeFlag;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, final ViewGroup container,
 							 Bundle savedInstanceState) {
@@ -49,6 +56,8 @@ public class TemplateFragment extends Fragment{
 		View rootView = inflater.inflate(R.layout.activity_test_template_list, container, false);
 		ButterKnife.bind(this,rootView);
 
+        imgDelete.setVisibility(View.INVISIBLE);
+		deleteModeFlag=false;
 		templateViewNew = (TemplateViewNew) rootView.findViewById(R.id.templateView);
 //		btnNext = (ImageView) rootView.findViewById(R.id.btnNext);
 //		btnPrevious = (ImageView) rootView.findViewById(R.id.btnPrevious);
@@ -78,7 +87,7 @@ public class TemplateFragment extends Fragment{
         templates=tDAO.getAllTemplate();
 
 
-        TemplateGridViewAdapter adapter=new TemplateGridViewAdapter(getActivity(),templates);
+		adapter = new TemplateGridViewAdapter(getActivity(),templates);
         gvList.setAdapter(adapter);
     }
 
@@ -90,9 +99,34 @@ public class TemplateFragment extends Fragment{
 
     @OnClick(R.id.template_list_img_new_template)
 	void newTemplate(){
-        Intent goAddTemplate=new Intent(getActivity(),AddNewTemplateActivity.class);
-        startActivity(goAddTemplate);
+		if(templates.size()<10){
+			Intent goAddTemplate=new Intent(getActivity(),AddNewTemplateActivity.class);
+			startActivity(goAddTemplate);
+		}
+		else
+			Toast.makeText(getActivity(), "You can't have more than 10 templates", Toast.LENGTH_SHORT).show();
+
 	}
+	@OnClick(R.id.template_list_minus)
+	void toggleDeleteMode(){
+		deleteModeFlag=!deleteModeFlag;
+		adapter.setEditingMode(deleteModeFlag);
+		gvList.setAdapter(adapter);
+        if(deleteModeFlag)
+            imgDelete.setVisibility(View.VISIBLE);
+        else
+            imgDelete.setVisibility(View.INVISIBLE);
+
+	}
+    @OnClick(R.id.template_list_img_delete)
+    void deleteSelectedTemplate(){
+        Log.i(TAG, "deleteSelectedTemplate: fired");
+        List<Integer> selectedIds=adapter.getSelectedList();
+        tDAO.deleteTemplate(selectedIds);
+		imgDelete.setVisibility(View.GONE);
+		deleteModeFlag=false;
+        updateAdapter();
+    }
 
 
 
